1103992223998d045d3c17ded67c27edd884f4a0
Ichika <131859874+ichika06@users.noreply.github.com>
2025-05-07 15:29:41 +0800

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Client-side Supabase Client (using anon key)
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = "https://ckwdowynrurrqbrawkxb.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrd2Rvd3lucnVycnFicmF3a3hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzOTAwNjEsImV4cCI6MjA2MDk2NjA2MX0.7fy2cgdFPJIwD9S4cSqlalb-wv7qKFC0Kt8SNMRnKkI";
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
